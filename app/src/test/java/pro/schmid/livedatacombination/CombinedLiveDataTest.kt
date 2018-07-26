7cf56710be1cd99cf2ddc129c5c6d1c909c922d9
Jonas Schmid <jonas.schmid@gmail.com>
2018-07-26 14:09:18 +0200

    Observe LiveData to get the values

--- app/src/test/java/pro/schmid/livedatacombination/CombinedLiveDataTest.kt
+++ app/src/test/java/pro/schmid/livedatacombination/CombinedLiveDataTest.kt
@@ -1,7 +1,13 @@
 package pro.schmid.livedatacombination
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.verify
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
 import org.junit.Rule
@@ -33,12 +39,15 @@ class CombinedLiveDataTest {
 
         val combined = CombinedLiveData.combine(data1, data2) { left, right -> left * right }
 
-        assertNull(combined.value)
+        val observer = mock<Observer<Int>>()
+        combined.observeForever(observer)
+
+        verify(observer, never()).onChanged(any())
 
         data1.value = 2
         data2.value = 3
 
-        assertEquals(6, combined.value)
+        verify(observer).onChanged(6)
     }
 
 }
